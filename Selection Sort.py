5f2a3e50707362c0ccc930b98349cafdefc60869
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-01-04 15:51:42 -0800

    Added selection sort

---
+++ Selection Sort.py
@@ -0,0 +1,37 @@
+from time import time
+from random import randint
+
+start = time()
+
+def selection(arr):
+    print(arr)
+    lowerbound, upperbound = 0, len(arr)-1
+    while lowerbound < upperbound:
+        cur_min_index, cur_max_index = lowerbound, upperbound
+        for i in range(lowerbound,upperbound+1):
+            if arr[i] < arr[cur_min_index]: cur_min_index = i
+            elif arr[i] > arr[cur_max_index]: cur_max_index = i
+        
+        arr[lowerbound],arr[cur_min_index] = arr[cur_min_index],arr[lowerbound]
+        
+        if cur_max_index == lowerbound: cur_max_index=cur_min_index # Handles the case where the highest number had been in the lowest slot
+        
+        arr[upperbound],arr[cur_max_index] = arr[cur_max_index],arr[upperbound]
+        lowerbound += 1
+        upperbound -= 1
+      
+randomlist = [randint(0,10000) for x in range(1000)]
+selection(randomlist)
+aftersort = sorted(randomlist)
+
+if randomlist != aftersort:
+    print("The sort didn't work!") # Sanity check
+    for a in range(len(randomlist)):
+        if randomlist[a] != aftersort[a]: print(a,randomlist[a],aftersort[a])
+
+
+print(randomlist)
+
+
+print("All done in {} seconds!".format(time()-start))
+
